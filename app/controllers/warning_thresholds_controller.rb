4786a056d933b66c032702fd5289a5dfe9891ecc
Thomas Osugi <thomas.osugi@snapdocs.com>
2021-11-30 15:25:24 -0700

    Add new threshold

--- app/controllers/warning_thresholds_controller.rb
+++ app/controllers/warning_thresholds_controller.rb
@@ -7,4 +7,24 @@ class WarningThresholdsController < ApplicationController
     @warning_threshold = WarningThreshold.find(params[:id])
     @pm25 = AqiService.call(@warning_threshold.city)
   end
+
+  def new
+    @warning_threshold = WarningThreshold.new
+  end
+
+  def create
+    @warning_threshold = WarningThreshold.new(warning_threshold_params)
+
+    if @warning_threshold.save
+      redirect_to @warning_threshold
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def warning_threshold_params
+    params.require(:warning_threshold).permit(:city, :threshold)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'warning_thresholds', to: 'warning_thresholds#index'
-  get 'warning_thresholds/:id', to: 'warning_thresholds#show'
+  root "warning_thresholds#index"
+
+  resources :warning_thresholds
 end
