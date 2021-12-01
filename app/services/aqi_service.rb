f413085fe558c2d5acbcf8a861a16292bb5457df
Thomas Osugi <thomas.osugi@snapdocs.com>
2021-11-30 20:55:26 -0700

    handle city with spaces in it

--- app/services/aqi_service.rb
+++ app/services/aqi_service.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 class AqiService
   BASE_URL = "http://api.waqi.info/feed".freeze
   TOKEN = "b8435bf86e53fd842f3a8ee9410ca37de428c3c3".freeze
@@ -16,7 +18,7 @@ class AqiService
   end
 
   def get_pm25(city)
-    response = conn.get(BASE_URL + '/' + city + '/?token=' + TOKEN)
+    response = conn.get("#{BASE_URL}/#{URI.escape(city)}/?token=#{TOKEN}")
     if response.body
       raw = JSON.parse(response.body)
       status = raw.dig('status')
